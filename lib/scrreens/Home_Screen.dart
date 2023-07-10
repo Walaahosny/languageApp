6109b2aa073b1c172394c09da5651c72a83261fe
Walaahosny <whussien82@gmail.com>
2023-07-11 02:16:24 +0300

    add navigTER TO SCREENS

--- lib/scrreens/Home_Screen.dart
+++ lib/scrreens/Home_Screen.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:languageapp/scrreens/phrases_screen.dart';
 import 'package:languageapp/widgets/container_FOR_home_screen.dart';
 import 'package:languageapp/widgets/widget_for_text.dart';
+
+import 'colors_screen.dart';
+import 'familymembers_screen.dart';
+import 'number_screen.dart';
 
 class Home_Screen extends StatelessWidget {
   const Home_Screen({super.key});
@@ -9,26 +14,50 @@ class Home_Screen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const text_style(text: 'Toku',),
+        title: const text_style(
+          text: 'Toku',
+        ),
         backgroundColor: const Color(0xff472d30),
       ),
-      body:  Column(
+      body: Column(
         children: [
           Container_Home_Screen(
             color: const Color(0xffffe1a8),
-            text: 'Numbers', pressed: () {  },
+            text: 'Numbers',
+            pressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return const Numbers_Screen();
+              }));
+            },
           ),
           Container_Home_Screen(
             color: const Color(0xffe26d5c),
-            text: 'Phrases', pressed: () {  },
+            text: 'Phrases',
+            pressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return const phrases_screen();
+              }));
+            },
           ),
           Container_Home_Screen(
             color: const Color(0xffc9cba3),
-            text: 'Family Members', pressed: () {  },
+            text: 'Family Members',
+            pressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const familyMembers_screen()));
+            },
           ),
           Container_Home_Screen(
             color: const Color(0xff723d46),
-            text: ' Colors', pressed: () { print("object"); },
+            text: ' Colors',
+            pressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const colors_screen()));
+            },
           )
         ],
       ),
